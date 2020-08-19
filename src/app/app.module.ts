7edf989dc9d0aa6b5881a24ad854163138b74eb7
Zbda <koulkzbda@gmail.com>
2020-08-19 16:26:40 +0200

    04-Compnent-Task: create component task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TopBarAppComponent } from './top-bar-app/top-bar-app.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarAppComponent
+    TopBarAppComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule
